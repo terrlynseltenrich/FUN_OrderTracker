be9d38f27b0186c2d93c171305769cf44ca2ba39
ebreti422 <evanbreti@gmail.com>
2025-10-01 21:22:06 -0600

    add mvc template 2nd try

---
+++ FUN_OrderTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FUN_OrderTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
